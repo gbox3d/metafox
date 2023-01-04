b737c84711200f88f09ea203a2c36329e96fdedc
gbox3d <gbox3d@gmail.com>
2023-01-04 13:44:34 +0900

    tcp lib

--- Apps/veuro_client/capent.py
+++ Apps/veuro_client/capent.py
@@ -37,83 +37,9 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 # import udpServer
 from modules.dariusVision import lastest_Cam2
 from modules.etc import isnotebook
-
-class senderClass:
-    checkcode = 20221223
-    
-    def __init__(self,ip,port,buff_size,bankId) :
-        self.ip = ip
-        self.port = port
-        self.buff_size = buff_size
-        self.bankId = bankId
-    def connect(self) :
-        try :
-            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.connect((self.ip, self.port))
-            self.client_socket.settimeout(60)
-            
-            _result = self.client_socket.recv(1024)
-            _code,_cmd,_,_,_ = struct.unpack('<LBBBB', _result)
-            print(f'code : {_code}, cmd : {_cmd}')
-            
-            if _cmd == 0 :
-                print('connected ok')
-            else :
-                print('connected fail : error code ' , _cmd)
-        except Exception as ex:
-            print(f'error : {ex}')
-    def send_ping(self) :
-        _packpack = struct.pack('<LBBBB', self.checkcode,0x10,0,0,0)
-        self.client_socket.sendall(_packpack)
-        _result = self.client_socket.recv(1024)
-        _code,_cmd,_,_,_ = struct.unpack('<LBBBB', _result)
-        print(f'code : {_code}, cmd : {_cmd}')
-        
-    def send_data(self,data) :
-        _data = data
-        _header = struct.pack('<LBBBBLffLLL', self.checkcode,0x01,self.bankId,0,0,len(_data),0,0,0,0,0)
-        self.client_socket.sendall(_header)
-        self.client_socket.sendall(_data)
-            
-        # time.sleep(60)
-            
-        # _result = self.client_socket.recv(1024)
-        
-        # # print(f'result : {len(_result)}')
-        # _code,_cmd,_,_,_,buff_size = struct.unpack('<LBBBBL', _result)
-        # print(f'code : {_code}, cmd : {_cmd} , buff_size : {buff_size}')
-        
-        # return _cmd,_,_,_,buff_size
-        
-    def download(self) :
-        
-        _header = struct.pack('<LBBBB', self.checkcode,0x02,self.bankId,0,0)
-        self.client_socket.sendall(_header)
-        
-        _result = self.client_socket.recv(1024)
-        
-        #read header
-        _code,_cmd,_,_,_,buff_size = struct.unpack('<LBBBBL', _result[0:12])
-        print(f'code : {_code}, cmd : {_cmd} , buff_size : {buff_size}')
-        
-        #read data
-        _data = _result[12:]
-        while len(_data) < buff_size :
-            _data += self.client_socket.recv(1024)
-        
-        # display(Image.open(io.BytesIO(_data)))
-        
-        
-    def close(self) :
-        _packpack = struct.pack('<LBBBB', self.checkcode,0x99,0,0,0)
-        self.client_socket.sendall(_packpack)
-        _result = self.client_socket.recv(1024)
-        _code,_cmd,_,_,_ = struct.unpack('<LBBBB', _result)
-        print(f'code : {_code}, cmd : {_cmd}')
-        self.client_socket.close()
+from veuroClientLib import veroTcpClient
 
 #%%
-
 with open( Path.joinpath( ROOT ,'Apps/neukey/config.yaml') , 'r') as f:
     config_data = yaml.load(f,Loader=yaml.FullLoader)
     print(config_data)
@@ -133,9 +59,9 @@ if source.isnumeric() :
 else:
     print('source is not numeric' , source)
 
-
 try :
-    senderObj = senderClass(remoteIP,remotePort,1024,bankId)
+    senderObj = veroTcpClient(remoteIP,remotePort,
+                              buff_size=1024,bankId=bankId,timeout=0.5)
     senderObj.connect()
     
     time.sleep(1)
@@ -155,19 +81,16 @@ try :
             _,_encodee_img = cv.imencode('.png',frame)
             
             startTime = time.time()
-            senderClass.send_data(senderObj,_encodee_img.tobytes())
+            # senderClass.send_data(senderObj,_encodee_img.tobytes())
+            senderObj.send_data(_encodee_img.tobytes())
             delay = time.time() - startTime
             print(f'network delay : {delay} sec , size : {len(_encodee_img.tobytes())}')
             
             cv.imshow('frame',frame)
             
-            
-            
-            # if isnotebook() :
-            #     display(Image.open(io.BytesIO(_encodee_img.tobytes())))
-            
             if cv.waitKey(1) == 27:
                 print('esc break')
+                
                 break
             
         except Exception as ex:
@@ -180,15 +103,17 @@ try :
         
 except Exception as ex:
     print(f'error : {ex}')
-    time.sleep(1)
+    # time.sleep(1)
     # exit()
 except KeyboardInterrupt :
     print('exiting....')
-    time.sleep(1)
+    # time.sleep(1)
+senderObj.close()
+time.sleep(1)
 
 time.sleep(1)
 if lcam != None:
     lcam.stop()
-senderObj.close()
+
 print('exit')
         
--- Apps/veuro_client/veuroClientLib.py
+++ Apps/veuro_client/veuroClientLib.py
@@ -7,9 +7,6 @@ import io
 from PIL import Image, ImageDraw, ImageFont
 from IPython.display import display
 
-
-checkcode = 20221223
-
 #%%
 with open('config.yaml', 'r') as f:
         
@@ -22,11 +19,14 @@ with open('config.yaml', 'r') as f:
 
 
 # %%
-class veroTestClient:
-    def __init__(self,ip,port,buff_size) :
+class veroTcpClient:
+    def __init__(self,ip,port,checkcode=20221223,buff_size=1024,bankId=0,timeout=1) :
         self.ip = ip
         self.port = port
         self.buff_size = buff_size
+        self.checkcode = checkcode
+        self.bankId = bankId
+        self.timeout = timeout
     def connect(self) :
         try :
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,59 +35,74 @@ class veroTestClient:
             print('connected ok')
             
             _result = self.client_socket.recv(1024)
-            _code,_cmd,_,_,_ = struct.unpack('<LBBBB', _result)
+            _code,_cmd = struct.unpack('<LB', _result[:5])
             print(f'code : {_code}, cmd : {_cmd}')
-            
             
         except Exception as ex:
             print(f'error : {ex}')
     def send_ping(self) :
-        _packpack = struct.pack('<LBBBB', checkcode,0x10,0,0,0)
+        _packpack = struct.pack('<LB', self.checkcode,0x10)
+        _packpack += bytearray(27) #padding
         self.client_socket.sendall(_packpack)
-        _result = self.client_socket.recv(1024)
-        _code,_cmd,_,_,_ = struct.unpack('<LBBBB', _result)
-        print(f'code : {_code}, cmd : {_cmd}')
-    def send_data(self) :
-        with open('test.png', 'rb') as f:
-            _data = f.read()
-            _header = struct.pack('<LBBBBLffLLL', checkcode,0x01,0,0,0,len(_data),0,0,0,0,0)
-            self.client_socket.sendall(_header)
-            self.client_socket.sendall(_data)
-            
-        _result = self.client_socket.recv(1024)
-        _code,_cmd,_,_,_,buff_size = struct.unpack('<LBBBBL', _result)
         
-        print(f'code : {_code}, cmd : {_cmd} , buff_size : {buff_size}')
+        _result = self.client_socket.recv(self.buff_size)
+        _code,_cmd = struct.unpack('<LB', _result[:5])
+        print(f'code : {_code}, cmd : {_cmd}')
+        
+    def send_data(self,data) :
+        # with open('test.png', 'rb') as f:
+        #     _data = f.read()
+        #     _header = struct.pack('<LBBBBL', self.checkcode,0x01,0,0,0,len(_data))
+        #     _header += bytearray(20) #padding 12+20 = 32
+        #     self.client_socket.sendall(_header)
+        #     self.client_socket.sendall(_data)
+        _data = data
+        _header = struct.pack('<LBBBBL', self.checkcode,0x01,self.bankId,0,0,len(_data))
+        _header += bytearray(20) #padding 12+20 = 32
+        self.client_socket.sendall(_header)
+        self.client_socket.sendall(_data)
+        
+        print('send data ok')
+            
+        # _result = self.client_socket.recv(1024)
+        # _code,_cmd,_,_,_,buff_size = struct.unpack('<LBBBBL', _result)
+        # print(f'code : {_code}, cmd : {_cmd} , buff_size : {buff_size}')
+        
     def download(self) :
         
-        _header = struct.pack('<LBBBB', checkcode,0x02,0,0,0)
+        _header = struct.pack('<LBB', self.checkcode,0x02,self.bankId)
+        _header += bytearray(26) #padding 12+20 = 32
         self.client_socket.sendall(_header)
         
-        _result = self.client_socket.recv(1024)
+        
+        _result = self.client_socket.recv(self.buff_size)
         
         #read header
         _code,_cmd,_,_,_,buff_size = struct.unpack('<LBBBBL', _result[0:12])
         print(f'code : {_code}, cmd : {_cmd} , buff_size : {buff_size}')
         
         #read data
-        _data = _result[12:]
+        _data = _result[32:]
         while len(_data) < buff_size :
-            _data += self.client_socket.recv(1024)
+            _data += self.client_socket.recv(self.buff_size)
         
         display(Image.open(io.BytesIO(_data)))
         
         
     def close(self) :
-        _packpack = struct.pack('<LBBBB', checkcode,0x99,0,0,0)
+        _packpack = struct.pack('<LB', self.checkcode,0x99)
+        _packpack += bytearray(27) #padding
         self.client_socket.sendall(_packpack)
-        _result = self.client_socket.recv(1024)
-        _code,_cmd,_,_,_ = struct.unpack('<LBBBB', _result)
+        
+        _result = self.client_socket.recv(self.buff_size)
+        _code,_cmd = struct.unpack('<LB', _result[:5])
         print(f'code : {_code}, cmd : {_cmd}')
         self.client_socket.close()
         
 #%%
 if __name__ == '__main__' :
-    _sender = veroTestClient(server_ip,port,buff_size)
+    checkcode = 20221223
+    _sender = veroTcpClient(server_ip,port,checkcode,buff_size)
     while True :
         _cmd = input('cmd : ')
         try :
@@ -101,7 +116,11 @@ if __name__ == '__main__' :
             elif _cmd == 'test1' :
                 _sender.connect()
                 _sender.send_ping()
-                _sender.send_data()
+                
+                with open('test.png', 'rb') as f:
+                    _data = f.read()
+                    _sender.send_data(_data)
+                    
                 _sender.close()
             elif _cmd == 'test2' :
                 _sender.connect()
